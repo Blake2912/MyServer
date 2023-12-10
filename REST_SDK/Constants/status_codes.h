d11c3c23e042ca73fa3a87d5cfd5ad44433343b0
Blake2912 <kcvarun29122001@gmail.com>
2023-12-11 01:05:56 +0530

    Add:  - Basic logic for GET and POST methods  - Constants folder to store all the constants that will be used in the SDK/Wrapper

---
+++ REST_SDK/Constants/status_codes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/*
+ * This is a constant class where we are storing all the possible status codes for our REST Wrapper
+ *
+ */
+
+namespace StatusCodes {
+    const int STATUS_CODE_200_OK = 200;
+    const int STATUS_CODE_500_ERR = 500;
+    const int STATUS_CODE_404_NOT_FOUND = 404;
+    const int STATUS_CODE_400_UNAUTHORIZED = 400;
+    const int STATUS_CODE_201_CREATED = 201;
+    const int STATUS_CODE_204_NO_CONTENT = 204;
+    
+}
